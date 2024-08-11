9135494ab8ed554cd68257ea4607b27fdf9c449c
kuties2013 <awssaratbm@gmail.com>
2024-08-11 20:07:27 +0530

    second commit

--- src/test/java/newwebdriver/NewTestng.java
+++ src/test/java/newwebdriver/NewTestng.java
@@ -5,5 +5,6 @@ import org.testng.annotations.Test;
 public class NewTestng {
   @Test
   public void f() {
+	  System.out.println("NewTesting only annotation");
   }
 }
---
+++ src/test/java/newwebdriver/SearchCase.java
@@ -0,0 +1,16 @@
+
+package newwebdriver;
+
+import org.testng.annotations.*;
+@Test
+public class SearchCase {
+	
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Searchcase with Test annotation");
+
+	}
+
+}
