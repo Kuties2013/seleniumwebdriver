7f497972285e47b53286e31555eec1bf60dbaf5a
kuties2013 <awssaratbm@gmail.com>
2024-08-12 12:21:44 +0530

    newly added

--- src/test/java/newwebdriver/FirstTestCase.java
+++ src/test/java/newwebdriver/FirstTestCase.java
@@ -1,14 +1,17 @@
 package newwebdriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class FirstTestCase {
-
+	@Test
 	public static void main(String[] args) {
 		
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://mvnrepository.com/artifact/org.testng/testng");
 		System.out.println("Successfully Launched");
+		Assert.assertEquals(false, null);
 		
 
 	}
